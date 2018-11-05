bd5124f47426618926eb8d77d9637747238abb0e
astroboy1 <omorim@hawaii.edu>
2018-11-05 01:39:44 +0000

    nohup preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,27 +1,27 @@
 import os
 import time
-import csv
 
 
 def main(directory="/images"):
     """Delete the images that are broken links, so aren't useful
     directory: directory containing the images to delete"""
     dir_path = os.path.dirname(os.path.realpath(__file__))
+    parent_dir = os.path.dirname(dir_path)
     threshold = 5000
-    print(dir_path)
-    deleted_files = []
     start = time.time()
-    for fn in os.listdir(dir_path + directory):
-        full_path = dir_path + directory + "/" + fn
+    num_images = 0
+    for fn in os.listdir(parent_dir + directory):
+        num_images += 1
+        full_path = parent_dir + directory + "/" + fn
         if os.stat(full_path).st_size < threshold:
             os.remove(full_path)
-            deleted_files.append(fn)
+            print("deleted file", fn)
+            print("Total files", num_images)
+            curr_time = time.time()
+            print("Time run so far", round(curr_time - start, "\n"))
     end = time.time()
     print("Time to delete files:", round(end - start), "seconds")
-    print("Number of deleted files:", len(deleted_files))
-    with open("deleted_files.csv", 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows([deleted_files])
+    print("Number of images", num_images)
 
 
 if __name__ == "__main__":
